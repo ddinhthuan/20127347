44c259e3653f22cb34118d6a87f6f2e549be921f
THUẬN NGUYỄN ĐÌNH <20127347@student.hcmus.edu.vn>
2024-01-02 18:50:38 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 88;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+    res.send('IF YOU CAN SEE THIS LINE IT MEAN THE CONTAINER IS RUNNING SUCCESSFUL');
+});
+
+app.get('/newpage', (req, res) => {
+    
+    res.send("a very new page now");
+})
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
